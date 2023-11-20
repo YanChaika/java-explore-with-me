4464edc79fab23d56112df7f9b48a4f38df0b44f
Ian <ian1990@yandex.ru>
2023-11-20 16:13:41 +0300

    ДП. Этап 1, перенести все изменения из ветки main в ветку stat_svc

---
+++ dto/src/main/java/org/example/explorewithme/dto/ViewStatsDto.java
@@ -0,0 +1,16 @@
+package org.example.explorewithme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ViewStatsDto {
+    private String app;
+    private String uri;
+    private Integer hits;
+}
---
+++ stats/src/main/java/org/example/explorewithme/model/EndpointHit.java
@@ -0,0 +1,23 @@
+package org.example.explorewithme.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity//(name = "EndpointHit")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "endpoint_hit")
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String app;
+    private String uri;
+    private String ip;
+    @Column(name = "times_tamp")
+    private LocalDateTime timestamp;
+}
