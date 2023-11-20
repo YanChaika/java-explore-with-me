eab9be9810921b4daa0f2f8bcbdb65c261ce73a0
Ian <ian1990@yandex.ru>
2023-11-20 16:41:09 +0300

    ДП. Этап 1, перенести все изменения из ветки main в ветку stat_svc

---
+++ stats/src/main/java/org/example/explorewithme/controller/StatsController.java
@@ -0,0 +1,38 @@
+package org.example.explorewithme.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.explorewithme.dto.EndpointHitDto;
+import org.example.explorewithme.dto.ViewStatsDto;
+import org.example.explorewithme.service.StatsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class StatsController {
+    private final StatsService statsService;
+
+    @GetMapping
+    public ResponseEntity<Optional<ViewStatsDto>> findStats(@NotNull @RequestParam String start,
+                                                            @NotNull @RequestParam String end,
+                                                            @RequestParam(name = "uris", defaultValue = "false") String uris,
+                                                            @RequestParam(name = "unique", defaultValue = "false") String unique
+    ) {
+        return ResponseEntity.ok(statsService.findStats(start, end, uris, unique));
+    }
+
+    @PostMapping
+    public ResponseEntity<Optional<EndpointHitDto>> postStats(@RequestBody @Valid EndpointHitDto endpointHit) {
+        return ResponseEntity.ok(statsService.createStats(endpointHit));
+    }
+}
